24a98290e12aa81565a739706bf9d18ed046d894
harika <kotakondaharika1995@gmail.com>
2019-11-04 21:56:42 +0530

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void Nativeapp()
+	{
+		System.out.println("Nativeapp");
+	}
+	@Test
+	public void Iosapp()
+	{
+		System.out.println("Iosapp");
+	}
+}
